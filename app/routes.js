8a027ab729fa68b587c04d2f19c7173f1639f609
BSH-PINS <baylee.sowter-halling@planninginspectorate.gov.uk>
2024-02-22 11:23:10 +0000

    Add confirmation

--- app/routes.js
+++ app/routes.js
@@ -63,3 +63,7 @@ router.post('/new/juggling-objects', (req, res) => {
     res.redirect('/new/check')
     }
 })
+
+router.post('/new/check', (req, res) => {
+    res.redirect('/new/confirmation')
+    })
